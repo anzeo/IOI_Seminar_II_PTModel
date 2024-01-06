b272116842fabeea9eba69f9eee5f0b30645891c
anzeo <anze.oblak2@gmail.com>
2024-01-06 21:26:22 +0100

    Helper endpoint to collect test data

--- server.py
+++ server.py
@@ -27,5 +27,34 @@ def train_model_request():
         return jsonify({'status': 'error', 'message': str(e)})
 
 
+@app.route('/storeTestData', methods=['POST'])
+def store_test_data():
+    import json
+
+    try:
+        directory = "data/test"
+        files = os.listdir(directory)
+        numeric_files = [int(os.path.splitext(f)[0]) for f in files if os.path.splitext(f)[0].isdigit() and f.lower().endswith('.json')]
+
+        # Find the file with the highest number
+        if numeric_files:
+            max_number = max(map(int, numeric_files))
+        else:
+            max_number = 0
+
+        # Increment the highest number by one
+        new_number = max_number + 1
+
+        # Create a new file with the incremented number
+        new_file_path = os.path.join(directory, str(new_number) + '.json')  # Adjust the file extension as needed
+
+        with open(new_file_path, 'w') as json_file:
+                json.dump(request.get_json(), json_file, indent=4)
+        return jsonify({'status': 'success', 'message': "Stored data in file " + new_file_path})
+
+    except Exception:
+        return jsonify({'status': 'error', 'message': "An error occurred while storing the data"})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
